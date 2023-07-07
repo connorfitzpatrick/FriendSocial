5454b98878b375f8fb59a11b8561571a33f5b594
connorfitzpatrick <connorfitz429@gmail.com>
2023-07-06 20:11:29 -0500

    UI progressing slowly as I figure out Angular. header and sidemenu are taking shape. Need to work on feed

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    SidemenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:"home", component:HomeComponent},
+      // Automatically direct to homepage if path value is empty
+      {path:'',redirectTo:'/home', pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
